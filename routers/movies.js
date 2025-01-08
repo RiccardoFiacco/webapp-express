c76b4490d22fb4633842bc01c6c1b16baff9e224
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-08 15:38:07 +0100

    add two routes: show and index

--- routers/movies.js
+++ routers/movies.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router();
 const moviesController = require('../controller/moviesController.js');
-
+const { existsId } = require('../middleware/utils.js');
 //index
 router.get('/', moviesController.index);
 //show
-router.get('/:id', moviesController.show);
+router.get('/:id', existsId, moviesController.show);
 
 module.exports = router
