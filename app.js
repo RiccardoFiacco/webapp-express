f5ca56b5e208a14e50d261ab65b4fbe610b7d798
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-08 15:09:57 +0100

    done the app

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors')
 const server = express()
-const port = 3000;
-server.get('/', (req, res)=>{
-    res.send('tutto ok')
-})
+const port = process.env.PORT;
+const moviesRouter = require('./routers/movies')
+
+server.use(express.static('public'))
+server.use(express.json())
+server.use(cors({origin:process.env.CORS_ORIGIN}))
+server.use('/api/movies', moviesRouter)
 server.listen(port,()=>{
     console.log("server in ascolto")
 })
