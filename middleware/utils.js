d440509ef84c280fbc80497caf03b00323effa5c
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-08 15:43:17 +0100

    add comment

--- middleware/utils.js
+++ middleware/utils.js
@@ -11,7 +11,7 @@ function existsId(req, res, next){
         if(err){
             return results.status(500).json({ error: 'Database query failed' });
         }
-        if(results.length === 0) {
+        if(results.length === 0) { //se il risultato è zero cioè nessun elemento preso mandiamo un messaggio di errore
             return res.status(404).json({
                     error: 'Not Found',
                     message: 'movie not found',
