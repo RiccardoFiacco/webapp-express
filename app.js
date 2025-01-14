15a56e94dcd1d58dc78b418dd8f0aa18668e9774
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-01-14 18:43:50 +0100

    add first try for user management

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const server = express()
 const port = process.env.PORT;
 const moviesRouter = require('./routers/movies');
 const reviewsRouter = require('./routers/reviews');
+const userRouter = require('./routers/user');
 const { errorsHandler, notFound } = require('./middleware/utils');
 
 server.use(express.static('public'))
@@ -11,6 +12,7 @@ server.use(express.json())
 server.use(cors({origin:process.env.CORS_ORIGIN}))
 server.use('/api/movies', moviesRouter)
 server.use('/api/reviews', reviewsRouter)
+server.use('/api/login', userRouter)
 server.use(errorsHandler)
 server.use(notFound)
 
--- routers/users.js
+++ routers/users.js
@@ -1,1 +1,12 @@
+const express = require('express')
+const router = express.Router();
+const userController = require('../controller/userController.js');
+const { checkInputReviews, checkValueInputReviews, trimString} = require('../middleware/utils.js');
+
 //index
+router.get('/', (req, res)=> {
+    console.log(req.body)
+    res.send("get degli user")
+});
+
+module.exports = router
