b1d0cb996901009e64f2b76a9e26fcd0cd5e9e0f
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-02-11 14:13:10 +0100

    try

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const { errorsHandler, notFound } = require('./middleware/utils');
 
 server.use(express.static('public'))
 server.use(express.json())
-server.use(cors({origin:process.env.CORS_ORIGIN}))
+// server.use(cors({origin:process.env.CORS_ORIGIN}))
+server.use(cors({origin:"https://riccardofiacco.github.io"}))
 server.use('/api/movies', moviesRouter)
 server.use('/api/reviews', reviewsRouter)
 server.use('/api/users', userRouter)
