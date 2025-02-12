b7664fa37f746ad5946676137b808aac757133b9
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-02-12 19:39:09 +0100

    try

--- api/app.js
+++ api/app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const cors = require('cors')
 const server = express()
 const port = process.env.PORT;
-const moviesRouter = require('./routers/movies');
-const reviewsRouter = require('./routers/reviews');
-const userRouter = require('./routers/users');
-const { errorsHandler, notFound } = require('./middleware/utils');
+const moviesRouter = require('../routers/movies');
+const reviewsRouter = require('../routers/reviews');
+const userRouter = require('../routers/users');
+const { errorsHandler, notFound } = require('../middleware/utils');
 
 server.use(express.static('public'))
 server.use(express.json())
-// server.use(cors({origin:process.env.CORS_ORIGIN}))
+server.use(cors({origin:process.env.CORS_ORIGIN}))
 server.use(cors({origin:"https://riccardofiacco.github.io"}))
 server.use('/api/movies', moviesRouter)
 server.use('/api/reviews', reviewsRouter)
