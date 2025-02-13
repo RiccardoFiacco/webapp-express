2461f8275663388491accc19e01535df94687465
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-02-13 16:00:20 +0100

    test 3

--- api/index.js
+++ api/index.js
@@ -2,20 +2,20 @@ const express = require('express');
 const cors = require('cors')
 const server = express()
 const port = process.env.PORT;
-const moviesRouter = require('../routers/movies');
-const reviewsRouter = require('../routers/reviews');
-const userRouter = require('../routers/users');
+// const moviesRouter = require('../routers/movies');
+// const reviewsRouter = require('../routers/reviews');
+// const userRouter = require('../routers/users');
 const { errorsHandler, notFound } = require('../middleware/utils');
 
-// server.use(express.static('public'))
+server.use(express.static('public'))
 server.use(express.json())
 server.use(cors())
 // server.use(cors({origin:process.env.CORS_ORIGIN}))
 // server.use(cors({origin:"https://riccardofiacco.github.io"}))
 server.get("/", (req, res) => res.send("prova 2"));
-server.use('/api/movies', moviesRouter)
-server.use('/api/reviews', reviewsRouter)
-server.use('/api/users', userRouter)
+// server.use('/api/movies', moviesRouter)
+// server.use('/api/reviews', reviewsRouter)
+// server.use('/api/users', userRouter)
 server.use(errorsHandler)
 server.use(notFound)
 
